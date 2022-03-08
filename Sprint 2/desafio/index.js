a796a50e5b6008d84211cd9a8ebc51e97c88ae53
Tdroid <tdroid@brq.com>
2022-03-08 01:54:46 -0300

    Test fetch api

--- Sprint 2/desafio/index.js
+++ Sprint 2/desafio/index.js
@@ -1,7 +1,7 @@
 let Weight = prompt(`Qual seu peso?`).replace(',', '.')
 let statusW = false
 let statusH = false
-let years;
+let years = prompt(`Quantos anos você tem?`)
 
 while(statusW === false) {
     if(Weight === null) {
@@ -29,4 +29,39 @@ let imc = Weight / height ^ 2
 
 console.log(`O imc é ${imc.toFixed()}`)
 alert(`O imc é ${imc.toFixed()}`)
-document.querySelector('#calc').innerTEXT = `<p>Seu IMC é ${(imc.toFixed())}</p>`
+
+if(imc.toFixed <= 20) {
+    alert(`Recomendações: Inclua carboidratos simples em sua dieta, além de proteínas -
+    indispensáveis para ganho de massa magra. Procure um profissional .
+    `)
+
+    alert(`Nivel de risco: Muitas complicações de saúde como doenças pulmonares e
+    cardiovasculares podem estar associadas ao baixo peso.`)
+    // < 20
+} else if(imc.toFixed() >= 20 && imc.toFixed() <= 24) {
+    alert(`Recomendações: Mantenha uma dieta saudável e faça seus exames periódicos.`)
+
+    alert(`Nivel de risco: Seu peso está ideal para suas referências.`)
+
+} else if(imc.toFixed() >= 25 && imc.toFixed() <= 29) {
+    alert(`Recomendações: Adote um tratamento baseado em dieta balanceada, exercício físico
+    e medicação. A ajuda de um profissional pode ser interessante`)
+
+    alert(`Nivel de risco: Aumento de peso apresenta risco moderado para outras doenças
+    crônicas e cardiovasculares.`)
+
+} else if(imc.toFixed() >= 30 && imc.toFixed() >= 35) {
+    alert(`Recomendações: Adote uma dieta alimentar rigorosa, com o acompanhamento de um
+    nutricionista e um médico especialista (endócrino).`)
+
+   alert(`Nivel de risco: Quem tem obesidade vai estar mais exposto a doenças graves e ao
+    risco de mortalidade.`)
+
+} else {
+    alert(`Recomendações: Adote uma dieta alimentar rigorosa, com o acompanhamento de um
+    nutricionista e um médico especialista (endócrino).`)
+
+    alert(`Nivel de risco: O obeso mórbido vive menos, tem alto risco de mortalidade geral
+    por diversas causas`)
+}
+
