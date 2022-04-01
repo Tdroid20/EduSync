10cd328437431514dfcf438b00d22ba2f17bf608
Tdroid <tdroid@brq.com>
2022-04-01 01:04:30 -0300

    Segredinhos

--- Sprint 4/SyncTur/src/scripts/javascript/packages.js
+++ Sprint 4/SyncTur/src/scripts/javascript/packages.js
@@ -1,5 +1,4 @@
 "use strict";
-
 let pack = [];
 const consult = () => {
     let $url = `https://62361b7feb166c26eb2f488a.mockapi.io/pacotes`;
@@ -19,22 +18,161 @@ const consult = () => {
     });
 };
 consult();
+let resgisterBTN = `<button id="siginup" class="siginup" onclick="$register()">Cadastrar</button>`
+let rquestUpBTN = `<button id="save" class="siginup" onclick="$save()" value="false">Salvar</button>`
 const packagesLoad = () => {
     const div = document.getElementById('packagesList');
-    
+    let divInsert = ``;
     for (let i = 0; i < pack.length; i++) {
-        const meses = ["jan", "fev", "mar", "abr", "mai", "jun", "jul","ago","set","out","nov","dez"];
+        const meses = ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"];
         let data = new Date(pack[i].data);
-        let dataFormatada = (+ (data.getDate() ) + '/' + meses[(data.getMonth())] + '/' + data.getFullYear());
-        
-        div.innerHTML += `<div class="card" id="pacote${pack[i].nome}">
-    <p class="packageTitle">${pack[i].nome}</p>
-    <p class="packageDesc">${pack[i].descricao}</p>
-    <br>
-    <p class="packageDate">Data da viagem: ${dataFormatada}</p>
-    <div class="btn">
-        <button class="packageBtn edit">Editar</button>
-        <button class="packageBtn delete">Excluir</button>
-    </div>`;
+        let dataFormatada = (+(data.getDate() + 1) + '/' + meses[(data.getMonth())] + '/' + data.getFullYear());
+        divInsert += `<div class="card" id="${pack[i].id}">
+        <p class="packageTitle">${pack[i].nome}</p>
+        <p class="packageDesc">${pack[i].descricao}</p>
+        <br>
+        <p class="packageDate">Data da viagem: ${dataFormatada}</p>
+        <div class="btn">
+            <button class="packageBtn edit" onclick="$edit(${pack[i].id})">Editar</button>
+            <button class="packageBtn delete" onclick="$delete(${i})">Excluir</button>
+        </div>
+        </div>`;
+        div.innerHTML = divInsert;
+        const btn = document.getElementById('btFlex');
+        btn.innerHTML = resgisterBTN
     }
 };
+
+
+const $register = () => {
+    const inputName = document.getElementById('name');
+    const inputDate = document.getElementById('date');
+    const inputDesc = document.getElementById('description');
+    const inputRadio = document.querySelector('input[name="bah"]:checked');
+    let _name = inputName.value;
+    let _date = inputDate.value;
+    let _desc = inputDesc.value;
+    let _status = inputRadio.value;
+    const meses = ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"];
+    let data = new Date(_date);
+    let dataFormatada = (data.getFullYear() + '-' + (data.getMonth() + 1) + '-' + (data.getDate() + 1));
+    let dataAtual = new Date();
+    let dataAtualFormatada = (dataAtual.getFullYear() + '-' + (dataAtual.getMonth() + 1) + '-' + (dataAtual.getDate()));
+    if (_name === '')
+        return alert('O nome do pacote não foi informado');
+    if (_date === '') {
+        alert('A data de viagem não foi informada');
+    }
+    if (_desc === '') {
+        alert('A descrição não foi informada');
+    }
+    if (dataFormatada < dataAtualFormatada)
+        return alert(`Você informou uma data passada.`);
+    let newPackage = {
+        nome: _name,
+        descricao: _desc,
+        data: _date,
+        status: _status,
+        id: pack.length + 1
+    };
+    console.log(newPackage);
+    pack.push(newPackage);
+    packagesLoad();
+    // zerando inputs
+    let resName = document.getElementById('name');
+    let resDate = document.getElementById('date');
+    let resdesc = document.getElementById('description');
+    let resStatus = document.getElementById('inactive');
+    resName.value = '';
+    resDate.value = '';
+    resdesc.value = '';
+    resStatus.checked = true;
+    window.location.href = `http://localhost:3000/src/pages/home.html#${pack.length}`;
+};
+const $delete = (indice) => {
+    pack.splice(indice, 1);
+    packagesLoad();
+};
+
+let documentEdit = []
+let editMode = false
+let $save = () => {
+    editMode = false
+    console.log(editMode)
+    $editMode(documentEdit)
+}
+const $editMode = (edit) => {
+    console.log(editMode)
+    const inputName = document.getElementById('name');
+    const inputDate = document.getElementById('date');
+    const inputDesc = document.getElementById('description');
+    const inputRadio = document.querySelector('input[name="bah"]:checked');
+    
+    let _name = inputName.value;
+    let _date = inputDate.value;
+    let _desc = inputDesc.value;
+    let _status = inputRadio.value;
+
+    edit.nome = _name;
+    edit.data = _date;
+    edit.descricao = _desc
+    if(_status == "false") {
+        edit.status = false
+    } else if(_status == "true") {
+        edit.status = true
+    }
+
+    if(editMode == true) {
+        document.getElementById('btFlex').innerHTML = ``
+        document.getElementById('btFlex').innerHTML = rquestUpBTN
+        console.log(editMode)
+    } else {
+        document.getElementById('btFlex').innerHTML = ``
+        document.getElementById('btFlex').innerHTML = resgisterBTN;
+        let resName = document.getElementById('name');
+        let resDate = document.getElementById('date');
+        let resdesc = document.getElementById('description');
+        let resStatus = document.getElementById('inactive');
+        resName.value = '';
+        resDate.value = '';
+        resdesc.value = '';
+        packagesLoad();
+        console.log(editMode)
+    }
+    
+}
+const $edit = (id) => {
+    const inputName = document.getElementById('name');
+    const inputDate = document.getElementById('date');
+    const inputDesc = document.getElementById('description');
+    const inputRadio = document.querySelector('input[name="bah"]:checked');
+    
+    let edit = pack.filter(x => x.id == id)[0];
+    console.log(edit)
+    let _name = inputName.value;
+    let _date = inputDate.value;
+    let _desc = inputDesc.value;
+    let _status = inputRadio.value;
+
+    const meses = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
+    let DataEdit = Date.UTC(edit.data);
+    const formatYmd = date => date.toISOString().slice(0, 10);
+
+    console.log(formatYmd(new Date(edit.data)));
+    
+    document.getElementById('name').value = edit.nome;
+    document.getElementById('date').value = formatYmd(new Date(edit.data));
+    document.getElementById('description').value = edit.descricao;
+
+    if(_status == "false" || _status == false) {
+        document.querySelector("#inactive").checked
+    } else if(_status == "true" || _status == true) {
+        document.querySelector("#active").checked
+    }
+    window.location.href = `http://localhost:3000/src/pages/home.html#editLink`;
+    
+    editMode = true;
+    documentEdit = edit
+    console.log(editMode)
+    $editMode(edit)
+}
--- Sprint 4/SyncTur/src/scripts/typescript/packages.ts
+++ Sprint 4/SyncTur/src/scripts/typescript/packages.ts
@@ -25,21 +25,167 @@ const consult = () => {
 consult()
 
 const packagesLoad: any = () => {
-    const div: any = document.getElementById('packagesList')
+    const div: any = document.getElementById('packagesList');
+    let divInsert = ``;
 
     for (let i = 0; i < pack.length; i++) {
         const meses = ["jan", "fev", "mar", "abr", "mai", "jun", "jul","ago","set","out","nov","dez"];
         let data = new Date(pack[i].data);
-        let dataFormatada = (+ (data.getDate() ) + '/' + meses[(data.getMonth())] + '/' + data.getFullYear());
+        let dataFormatada = (+ (data.getDate() + 1 ) + '/' + meses[(data.getMonth())] + '/' + data.getFullYear());
         
-        div.innerHTML += `<div class="card" id="pacote${pack[i].nome}">
-    <p class="packageTitle">${pack[i].nome}</p>
-    <p class="packageDesc">${pack[i].descricao}</p>
-    <br>
-    <p class="packageDate">Data da viagem: ${dataFormatada}</p>
-    <div class="btn">
-        <button class="packageBtn edit">Editar</button>
-        <button class="packageBtn delete">Excluir</button>
-    </div>`;
+        divInsert += `<div class="card" id="${pack[i].id}">
+        <p class="packageTitle">${pack[i].nome}</p>
+        <p class="packageDesc">${pack[i].descricao}</p>
+        <br>
+        <p class="packageDate">Data da viagem: ${dataFormatada}</p>
+        <div class="btn">
+            <button class="packageBtn edit" onclick="$edit(${i})">Editar</button>
+            <button class="packageBtn delete" onclick="$delete(${i})">Excluir</button>
+        </div>
+        </div>`
+        div.innerHTML = divInsert;
     }
 }
+
+
+interface Ipacks {
+    nome: string;
+    descricao: string;
+    data: Date
+    status: boolean;
+    id: number;
+}
+
+const $register = () => {
+    const inputName: any = document.getElementById('name');
+    const inputDate: any = document.getElementById('date');
+    const inputDesc: any = document.getElementById('description')
+    const inputRadio: any = document.querySelector('input[name="bah"]:checked');
+
+    let _name: string = inputName.value;
+    let _date: any = inputDate.value;
+    let _desc: string = inputDesc.value;
+    let _status: boolean = inputRadio.value;
+    
+    const meses = ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"];
+    let data = new Date(_date);
+    let dataFormatada = (data.getFullYear() + '-' + (data.getMonth() +1) + '-' + (data.getDate() + 1));
+
+
+    let dataAtual = new Date();
+    let dataAtualFormatada = (dataAtual.getFullYear() + '-' + (dataAtual.getMonth() +1) + '-' + (dataAtual.getDate()));
+    if (_name === '')
+        return alert('O nome do pacote não foi informado');
+    if (_date === '') {
+        alert('A data de viagem não foi informada');
+    }
+    if (_desc === '') {
+        alert('A descrição não foi informada');
+    }
+
+    if (dataFormatada < dataAtualFormatada)
+        return alert(`Você informou uma data passada.`);
+
+
+    let newPackage: Ipacks = {
+        nome: _name,
+        descricao: _desc,
+        data: _date,
+        status: _status,
+        id: pack.length + 1
+    }
+    console.log(newPackage);
+    pack.push(newPackage);
+    packagesLoad();
+
+    // zerando inputs
+
+    let resName: any = document.getElementById('name');
+    let resDate: any = document.getElementById('date');
+    let resdesc: any = document.getElementById('description');
+    let resStatus: any = document.getElementById('inactive');
+    resName.value = '';
+    resDate.value = '';
+    resdesc.value = '';
+    resStatus.checked = true;
+
+    window.location.href = `http://localhost:3000/src/pages/home.html#${pack.length}`
+} 
+
+const $delete = (indice: number) => {
+    pack.splice(indice, 1)
+    packagesLoad()
+}
+
+
+const $edit = (id: number) => {
+    const inputName: any = document.getElementById('name');
+    const inputDate: any = document.getElementById('date');
+    const inputDesc: any = document.getElementById('description');
+    const inputRadio: any = document.querySelector('input[name="bah"]:checked');
+    
+    let edit: any = pack.filter(x => x.id == id);
+
+    let modfy = edit[0]
+    let _name: string = inputName.value;
+    let _date: string = inputDate.value;
+    let _desc: string = inputDesc.value;
+    let _Status: boolean = inputRadio.value;
+    
+    _name = modfy.nome;
+    console.log(_name)
+    _date = edit.data;
+    _desc = edit.descricao;
+    _Status = edit.status;
+    
+    const meses = ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"];
+    let data = new Date(_date);
+    let dataFormatada = (data.getFullYear() + '-' + (data.getMonth() + 1) + '-' + (data.getDate() + 1));
+    let dataAtual = new Date();
+    let dataAtualFormatada = (dataAtual.getFullYear() + '-' + (dataAtual.getMonth() + 1) + '-' + (dataAtual.getDate()));
+
+    
+
+    if(_name === '')
+        return alert('O nome do pacote não foi informado');
+    if(_date === '') {
+        alert('A data de viagem não foi informada');
+    }
+    if(_desc === '') {
+        alert('A descrição não foi informada');
+    }
+    if(dataFormatada < dataAtualFormatada)
+        return alert(`Você informou uma data passada.`);
+
+
+
+
+        window.location.href = `http://localhost:3000/src/pages/home.html#main`
+}
+
+/* 
+
+const $edit = (id) => {
+    const inputName = document.getElementById('name');
+    const inputDate = document.getElementById('date');
+    const inputDesc = document.getElementById('description');
+    const inputRadio = document.querySelector('input[name="bah"]:checked');
+    
+    let edit = pack.filter(x => x.id == id);
+    edit = edit[0];
+    let _name = inputName.value;
+    let _date = inputDate.value;
+    let _desc = inputDesc.value;
+    let _Status = inputRadio.value;
+    console.log(edit.data)
+    let DataEdit = new Date(edit.data);
+    console.log(DataEdit)
+    let dataFormatadaEdit = (DataEdit.getFullYear() + '-' + (DataEdit.getMonth() + 1) + '-' + (DataEdit.getDate() + 1));
+    
+    document.getElementById('name').value = edit.nome;
+    document.getElementById('date').value = dataFormatadaEdit;
+    document.getElementById('description').value = edit.descricao;
+    document.querySelector('inctive') = edit.status;
+    window.location.href = `http://localhost:3000/src/pages/home.html#main`
+    
+} */
