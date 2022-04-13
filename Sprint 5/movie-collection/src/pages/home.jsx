2ee0aee7534e3b870459324242f5e1268ae69370
Tdroid <tdroid@brq.com>
2022-04-13 00:50:04 -0300

    Challange 5

--- Sprint 5/movie-collection/src/pages/home.jsx
+++ Sprint 5/movie-collection/src/pages/home.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import './css/maintenance.css';
+import logo from '../assets/logoColored.png'
 
 function Home() {
     return (
         <div>
-            <h1>Em desenvolvimento</h1>
+            <head>
+            <link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet" />
+            </head>
+            <div className="main">
+            <div class="box">
+                <img src={logo} id="logo"/>
+                <div id="title">
+                SITE EM MANUTENÇÃO
+                </div>
+                <div id="descricao">
+                (21) 998656604<br />
+                tdroid@brq.com
+                </div>
+            </div>
+            </div>
         </div>
     )
 }
